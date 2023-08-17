5cd513a292cc51f6460e8ab60751c90d304445b4
Heidi Rajala <heidi.rajala@integrify.io>
2023-08-17 19:51:13 +0300

    How to create a book without creating a new author, if author input name only?

--- backend/LibraryApi.Service/src/Implementations/BookService.cs
+++ backend/LibraryApi.Service/src/Implementations/BookService.cs
@@ -9,8 +9,21 @@ namespace LibraryApi.Service.src.Implementations;
 public class BookService : BaseService<Book, BookReadDto, BookCreateDto, BookUpdateDto>, IBookService
 {
   private readonly IBookRepo _bookRepo;
-  public BookService(IBookRepo bookRepo, IMapper mapper) : base(bookRepo, mapper)
+  private readonly IAuthorRepo _authorRepo;
+  public BookService(IBookRepo bookRepo, IAuthorRepo authorRepo, IMapper mapper) : base(bookRepo, mapper)
   {
     _bookRepo = bookRepo;
+    _authorRepo = authorRepo;
   }
+
+    public override async Task<BookReadDto> CreateOne(BookCreateDto dto)
+    {
+      var author = await _authorRepo.FindAuthorByName(dto.Author.FirstName, dto.Author.LastName);
+      if (author is not null)
+      {
+        Console.WriteLine("Author found!");
+        Console.WriteLine(author.FirstName);
+      }
+      return await base.CreateOne(dto);
+    }
 }
