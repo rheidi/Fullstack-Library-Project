14615a53c2f87fee611e4b6e292d9ec7b016f88b
Heidi Rajala <heidi.rajala@integrify.io>
2023-10-11 17:36:29 +0300

    Create custom exceptions

---
+++ backend/LibraryApi.Service/src/Shared/CustomException.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace LibraryApi.Service.src.Shared;
+
+public class CustomException : Exception
+{
+  public int StatusCode { get; set; }
+  public string ErrorMessage { get; set; }
+
+  public CustomException(int statusCode = 500, string message = "Internal server error")
+  {
+    StatusCode = statusCode;
+    ErrorMessage = message;
+  }
+
+  public static CustomException NotFoundException(string message = "Item cannot be found")
+  {
+    return new CustomException(404, message);
+  }
+
+  public static CustomException BadRequest(string message = "Request parameters are invalid")
+  {
+    return new CustomException(400, message);
+  }
+}
