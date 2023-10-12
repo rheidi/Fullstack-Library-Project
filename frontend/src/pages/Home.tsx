d7e70e7b4eeef2dd6dd841a164a0f9942435541c
Heidi Rajala <heidi.rajala@integrify.io>
2023-10-12 15:47:19 +0300

    Edit palette to use colors from inspiration photo

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,9 @@ const Home = () => {
     <main>
       <h1>The good books library</h1>
       <p>This is an ever growing library of random books</p>
+      <p>This project backend is deployed in azure cloud services,
+        it is really slow to start, maybe due to the back end residing in low priority tier computing
+        because it is a free service. It might take up to 20-30 seconds to start, but then it should operate normally.</p>
     </main>
   )
 }
